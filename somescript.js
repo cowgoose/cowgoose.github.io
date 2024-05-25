a625723cad1a3767d76dd4359657de34437b5017
Kevin Lee <145483343+cowgoose@users.noreply.github.com>
2024-05-25 08:42:12 -0700

    fix lil bug

--- somescript.js
+++ somescript.js
@@ -9,13 +9,4 @@ setTimeout(function(){
     document.getElementById("socialdiv").style.visibility = "visible";
 }, 1300);
 
-let t = 0; // time variable
-
-function setup() {
-  createCanvas(600, 600);
-  noStroke();
-  fill(40, 200, 40);
-}
-
-const body = document.getElementsByTagName('body').style.background = draw();
 
